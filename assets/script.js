64df8c74a7d8d08231594d57acdd6586cdc235e0
Chelsey Morris <chelseymorris91@yahoo.com>
2021-02-22 20:58:33 -0800

    got text in input to save to local storage and current to console log

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,21 @@
+
 // displays current day
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
+$(document).ready(function() {
+
+    $(".saveBtn").on('click', function() {
+        var input = $(this).siblings(".textarea").val();
+        console.log(input);
+        localStorage.setItem('input', input);
+     })
+
+     function timeUpdater() {
+        var currentTime = moment().format("hh:mm:ss");
+        
+     }
+     
+   
+
+})
